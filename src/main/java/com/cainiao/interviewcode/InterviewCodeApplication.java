07d4c7ca3057da41a8c94b3ae5c6b792827f4d3d
xusizhuo <2356412520@qq.com>
2023-01-08 19:36:55 +0800

    Initial commit

---
+++ src/main/java/com/cainiao/interviewcode/InterviewCodeApplication.java
@@ -0,0 +1,13 @@
+package com.cainiao.interviewcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cainiao/interviewcode/InterviewCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cainiao.interviewcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
